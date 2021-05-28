e2d25ea5872e1d1a83fe5dad1f67e83840ea1597
Adrian <czarnywilczek631@gmail.com>
2021-05-28 11:36:08 +0200

    Zadanie 12 wykonane

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { Route, Switch, Redirect } from "react-router-dom";
+import Posts from "./components/posts";
+import NotFound from "./components/notFound";
+import Home from "./components/home";
+import LoginForm from "./components/loginForm";
+import SignUpForm from "./components/signUp";
+import NavBar from "./components/common/navbar";
+import { isExpired } from "react-jwt";
+
+function App() {
+
+  return (
+      <div className="container-fluid">
+        <NavBar />
+        <div className="container">
+            <div className="content">
+                <Switch>
+                    <Route path="/login" component={LoginForm} />
+                    <Route path="/posts"
+                            render={props => {
+                            if (isExpired(localStorage.getItem('token'))) {
+                                return <Redirect to="/" />;
+                            }
+                            return <Posts />;
+                        }}
+                    />
+
+                    <Route path="/signUp" component={SignUpForm} />
+                    <Route path="/not-found" component={NotFound} />
+                    <Route path="/" exact component={Home} />
+                    <Redirect to="/not-found" />
+                </Switch>
+            </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
